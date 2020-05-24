6d59061814bd9d1e760e515da2b4d8a8b52bbb41
vandananayak <62019858+vandananayak@users.noreply.github.com>
2020-05-24 11:42:38 +0530

    Data retrieval from web server
    Data retrieval from the web server without using urlib module

--- .github/workflows/Sockets.py
+++ .github/workflows/Sockets.py
@@ -1,11 +1,15 @@
 #1.import socket module
 import socket
+
 #2.create a socket name mysocket
 mysocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
 #3.connect that socket to the web page
 mysocket.connect(('data.pr4e.org',80))
-#4.get the web page, encode it and store it in cmnd
+
+#4.get the web page, encode the data in bytes and store it in cmnd
 cmnd='GET https://data.pr4e.org/romeo.txt HTTP/1.1\n\n'.encode()
+
 #5.send the cmnd through mysocket and request the web page
 mysocket.send(cmnd)
 
@@ -13,5 +17,5 @@ while(True):
     data=mysocket.recv(500)#receive 500 charecter
     if len(data)<1:#End of file
         break
-    print(data.decode())#decode 500 charecter to print
+    print(data.decode())#Byte is decoded to unicode string
 mysocket.close()#close socket connection
